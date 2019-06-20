e1dc079830829aa276331ed7363e5374eadab284
Fajkowsky <dawid.fajkowski@gmail.com>
2019-06-20 09:30:13 +0200

    Get rid of not needed code, add base components.

---
+++ src/components/ElementGroup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="element-group">
+    <p>element-group</p>
+    <element-item v-for="key in keys" :key="key"/>
+  </div>
+</template>
+
+<script>
+import ElementItem from '@/components/ElementItem.vue';
+
+export default {
+  name: 'ElementGroup',
+  components: { ElementItem },
+  data() {
+    return {
+      keys: [1, 2],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+  #element-group {
+  }
+</style>
---
+++ src/components/ElementItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="element-item">element-item</div>
+</template>
+
+<script>
+export default {
+  name: 'ElementItem',
+};
+</script>
+
+<style lang="scss">
+  #element-item {
+  }
+</style>
